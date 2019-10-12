b7e94072f695c7d563c6e1f4906e01538858b064
daiyu <deeyu54321@gmail.com>
2019-10-12 08:49:32 +0800

    feat： 新增vuetify组件

--- src/main.js
+++ src/main.js
@@ -2,13 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Button } from 'ant-design-vue'
+import { Button, carousel } from 'ant-design-vue'
 // import { Button } from 'element-ui'
 Vue.config.productionTip = false
 Vue.use(Button)
+Vue.use(carousel)
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
+// vuetify 自定义配置
+export default new Vuetify({})
 // Vue.use(ElementUI)
 new Vue({
   router,
   store,
+  vuetify: new Vuetify(),
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="app">
+    <v-card class="mx-auto" max-width="400">
+      <v-img
+        class="white--text"
+        height="200px"
+        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+      >
+        <v-card-title class="align-end fill-height"
+          >需要一句非主流的话</v-card-title
+        >
+      </v-img>
+
+      <v-card-text>
+        <v-form ref="form" v-model="valid" :lazy-validation="lazy">
+          <v-text-field
+            v-model="username"
+            :counter="10"
+            :rules="nameRules"
+            label="帐户名"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            :rules="passwordRules"
+            label="密码"
+            required
+            type="password"
+          ></v-text-field>
+        </v-form>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn color="primary" class="loginBtn" @click="login">
+          登录
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      valid: true,
+      lazy: false,
+      nameRules: [v => !!v || '请输入用户名'],
+      passwordRules: [v => !!v || '请输入密码'],
+      username: 'admin', // 用户名
+      password: '123456' // 密码
+    }
+  },
+  methods: {
+    login() {}
+    // 登录
+    // async login () {
+    //   if (this.$refs.form.validate()) {
+    //     const form = {
+    //       username: this.username,
+    //       password: this.password
+    //     }
+    //     await this.$store.dispatch('user/login', form)
+    //     this.$router.push({
+    //       path: '/'
+    //     })
+    //   }
+    // }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.app {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .loginBtn {
+    width: 80%;
+    margin: 0 auto 20px;
+  }
+}
+</style>
